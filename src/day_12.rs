36082e5e51e2c8e373f2ed1a08e539907a20b600
mor.verbin <mor.verbin@coolblue.nl>
2024-12-12 14:47:12 +0100

    Day 12: Letting part 1 make use of part 1 algo.

--- src/day_12.rs
+++ src/day_12.rs
@@ -107,39 +107,6 @@ fn get_regions(map: &[Vec<char>]) -> Vec<Vec<(usize, usize)>> {
     regions
 }
 
-fn part_1(file_path: String) -> u64 {
-    let map = parse_data(file_path);
-    let regions = get_regions(&map);
-
-    let mut result = 0;
-    for region in regions {
-        let region_size = region.len();
-        let mut fence_size = 0;
-
-        let region_hashset = region
-            .iter()
-            .copied()
-            .map(|(row, column)| (row as isize, column as isize))
-            .collect::<HashSet<_>>();
-
-        for (node_row, node_column) in region {
-            let neighbour_count = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-                .into_iter()
-                .map(|(d_row, d_column)| {
-                    (node_row as isize + d_row, node_column as isize + d_column)
-                })
-                .filter(|node| !region_hashset.contains(node))
-                .count();
-
-            fence_size += neighbour_count;
-        }
-
-        result += region_size * fence_size;
-    }
-
-    result as u64
-}
-
 #[derive(Copy, Clone, Debug, Eq, Hash, PartialEq)]
 enum Direction {
     Up,
@@ -148,76 +115,96 @@ enum Direction {
     Left,
 }
 
+fn get_edges(
+    region: &Vec<(usize, usize)>,
+) -> (
+    HashSet<(isize, isize)>,
+    HashSet<((isize, isize), (isize, isize), Direction)>,
+) {
+    let region_hashset = region
+        .iter()
+        .copied()
+        .map(|(row, column)| (row as isize, column as isize))
+        .collect::<HashSet<_>>();
+
+    let mut fence_points = HashSet::new();
+    let mut fence_edges = HashSet::new();
+
+    for (node_row, node_column) in region {
+        let (node_row, node_column) = (*node_row as isize, *node_column as isize);
+        let up_neighbour = (node_row - 1, node_column);
+        let down_neighbour = (node_row + 1, node_column);
+        let left_neighbour = (node_row, node_column - 1);
+        let right_neighbour = (node_row, node_column + 1);
+
+        if !region_hashset.contains(&up_neighbour) {
+            fence_points.insert((node_row, node_column));
+            fence_points.insert((node_row, node_column + 1));
+            fence_edges.insert((
+                (node_row, node_column),
+                (node_row, node_column + 1),
+                Direction::Up,
+            ));
+        }
+
+        if !region_hashset.contains(&down_neighbour) {
+            fence_points.insert((node_row + 1, node_column));
+            fence_points.insert((node_row + 1, node_column + 1));
+            fence_edges.insert((
+                (node_row + 1, node_column),
+                (node_row + 1, node_column + 1),
+                Direction::Down,
+            ));
+        }
+        if !region_hashset.contains(&left_neighbour) {
+            fence_points.insert((node_row, node_column));
+            fence_points.insert((node_row + 1, node_column));
+            fence_edges.insert((
+                (node_row, node_column),
+                (node_row + 1, node_column),
+                Direction::Left,
+            ));
+        }
+
+        if !region_hashset.contains(&right_neighbour) {
+            fence_points.insert((node_row, node_column + 1));
+            fence_points.insert((node_row + 1, node_column + 1));
+            fence_edges.insert((
+                (node_row, node_column + 1),
+                (node_row + 1, node_column + 1),
+                Direction::Right,
+            ));
+        }
+    }
+    (fence_points, fence_edges)
+}
+
+fn part_1(file_path: String) -> u64 {
+    let map = parse_data(file_path);
+    let regions = get_regions(&map);
+
+    let mut result = 0;
+    for region in regions {
+        let (_, fence_edges) = get_edges(&region);
+
+        result += region.len() * fence_edges.len();
+    }
+
+    result as u64
+}
+
 fn part_2(file_path: String) -> u64 {
     let map = parse_data(file_path);
     let regions = get_regions(&map);
 
     let mut result = 0;
     for region in regions {
-        let region_size = region.len();
-        let region_hashset = region
-            .iter()
-            .copied()
-            .map(|(row, column)| (row as isize, column as isize))
-            .collect::<HashSet<_>>();
+        let (fence_points, fence_edges) = get_edges(&region);
 
-        let mut fence_points = HashSet::new();
-        let mut fence_edges = HashSet::new();
-
-        for (node_row, node_column) in region {
-            let (node_row, node_column) = (node_row as isize, node_column as isize);
-            let up_neighbour = (node_row - 1, node_column);
-            let down_neighbour = (node_row + 1, node_column);
-            let left_neighbour = (node_row, node_column - 1);
-            let right_neighbour = (node_row, node_column + 1);
-
-            if !region_hashset.contains(&up_neighbour) {
-                fence_points.insert((node_row, node_column));
-                fence_points.insert((node_row, node_column + 1));
-                fence_edges.insert((
-                    (node_row, node_column),
-                    (node_row, node_column + 1),
-                    Direction::Up,
-                ));
-            }
-
-            if !region_hashset.contains(&down_neighbour) {
-                fence_points.insert((node_row + 1, node_column));
-                fence_points.insert((node_row + 1, node_column + 1));
-                fence_edges.insert((
-                    (node_row + 1, node_column),
-                    (node_row + 1, node_column + 1),
-                    Direction::Down,
-                ));
-            }
-            if !region_hashset.contains(&left_neighbour) {
-                fence_points.insert((node_row, node_column));
-                fence_points.insert((node_row + 1, node_column));
-                fence_edges.insert((
-                    (node_row, node_column),
-                    (node_row + 1, node_column),
-                    Direction::Left,
-                ));
-            }
-
-            if !region_hashset.contains(&right_neighbour) {
-                fence_points.insert((node_row, node_column + 1));
-                fence_points.insert((node_row + 1, node_column + 1));
-                fence_edges.insert((
-                    (node_row, node_column + 1),
-                    (node_row + 1, node_column + 1),
-                    Direction::Right,
-                ));
-            }
-        }
-
-        // println!("Fence points for region {region_hashset:?} are {fence_points:?}");
-        // println!("Fence edges for region {region_hashset:?} are {fence_edges:?}");
         let mut combined_fence_edges = HashSet::new();
         let mut fence_points_vec = fence_points.iter().copied().collect::<Vec<_>>();
 
         while let Some(point) = fence_points_vec.pop() {
-            // println!("Checking point {point:?}");
             for direction in [Direction::Up, Direction::Down] {
                 // Go left
                 let mut left_point = point;
@@ -228,7 +215,6 @@ fn part_2(file_path: String) -> u64 {
                 )) {
                     left_point = (left_point.0, left_point.1 - 1);
                 }
-                // println!("Went left to point {left_point:?}");
 
                 // Go right
                 let mut right_point = point;
@@ -239,7 +225,6 @@ fn part_2(file_path: String) -> u64 {
                 )) {
                     right_point = (right_point.0, right_point.1 + 1);
                 }
-                // println!("Went right to point {right_point:?}");
 
                 if right_point != left_point {
                     combined_fence_edges.insert((left_point, right_point));
@@ -252,7 +237,6 @@ fn part_2(file_path: String) -> u64 {
                 while fence_edges.contains(&((up_point.0 - 1, up_point.1), up_point, direction)) {
                     up_point = (up_point.0 - 1, up_point.1);
                 }
-                // println!("Went up to point {up_point:?}");
 
                 // Go down
                 let mut down_point = point;
@@ -263,7 +247,6 @@ fn part_2(file_path: String) -> u64 {
                 )) {
                     down_point = (down_point.0 + 1, down_point.1);
                 }
-                // println!("Went down to point {down_point:?}");
 
                 if up_point != down_point {
                     combined_fence_edges.insert((up_point, down_point));
@@ -271,9 +254,7 @@ fn part_2(file_path: String) -> u64 {
             }
         }
 
-        // println!("Combined fence edges for region {region_hashset:?} are {combined_fence_edges:?}");
-        // println!("So I'm going to add {} and {} for {}", region_size, combined_fence_edges.len(), region_size * combined_fence_edges.len());
-        result += region_size * combined_fence_edges.len();
+        result += region.len() * combined_fence_edges.len();
     }
 
     result as u64
