02e0516016e041fa578e9c9a5218cb0e3ad13385
mor.verbin <mor.verbin@coolblue.nl>
2024-12-12 11:24:17 +0100

    Day 12: Phew... I'll be spending quite a while simplifying this later, though.

--- src/day_12.rs
+++ src/day_12.rs
@@ -1,5 +1,4 @@
 use crate::file_utilities::read_lines;
-use itertools::Itertools;
 
 use std::collections::{HashSet, VecDeque};
 
@@ -22,7 +21,6 @@ pub fn run(file_path: String, part: i32) -> u64 {
         _ => panic!("... nope."),
     }
 }
-
 
 fn get_regions(map: Vec<Vec<char>>) -> Vec<Vec<(usize, usize)>> {
     let map_size = map.len() as isize;
@@ -102,12 +100,18 @@ fn part_1(file_path: String) -> u64 {
         let region_size = region.len();
         let mut fence_size = 0;
 
-        let region_hashset = region.iter().copied().map(|(row, column)| (row as isize, column as isize)).collect::<HashSet<_>>();
+        let region_hashset = region
+            .iter()
+            .copied()
+            .map(|(row, column)| (row as isize, column as isize))
+            .collect::<HashSet<_>>();
 
         for (node_row, node_column) in region {
             let neighbour_count = [(0, 1), (1, 0), (0, -1), (-1, 0)]
                 .into_iter()
-                .map(|(d_row, d_column)| (node_row as isize + d_row, node_column as isize + d_column))
+                .map(|(d_row, d_column)| {
+                    (node_row as isize + d_row, node_column as isize + d_column)
+                })
                 .filter(|node| !region_hashset.contains(node))
                 .count();
 
@@ -118,6 +122,14 @@ fn part_1(file_path: String) -> u64 {
     }
 
     result as u64
+}
+
+#[derive(Copy, Clone, Debug, Eq, Hash, PartialEq)]
+enum Direction {
+    Up,
+    Right,
+    Down,
+    Left,
 }
 
 fn part_2(file_path: String) -> u64 {
@@ -146,77 +158,109 @@ fn part_2(file_path: String) -> u64 {
             if !region_hashset.contains(&up_neighbour) {
                 fence_points.insert((node_row, node_column));
                 fence_points.insert((node_row, node_column + 1));
-                fence_edges.insert(((node_row, node_column), (node_row, node_column + 1)));
+                fence_edges.insert((
+                    (node_row, node_column),
+                    (node_row, node_column + 1),
+                    Direction::Up,
+                ));
             }
 
             if !region_hashset.contains(&down_neighbour) {
                 fence_points.insert((node_row + 1, node_column));
                 fence_points.insert((node_row + 1, node_column + 1));
-                fence_edges.insert(((node_row + 1, node_column), (node_row + 1, node_column + 1)));
+                fence_edges.insert((
+                    (node_row + 1, node_column),
+                    (node_row + 1, node_column + 1),
+                    Direction::Down,
+                ));
             }
             if !region_hashset.contains(&left_neighbour) {
                 fence_points.insert((node_row, node_column));
                 fence_points.insert((node_row + 1, node_column));
-                fence_edges.insert(((node_row, node_column), (node_row + 1, node_column)));
+                fence_edges.insert((
+                    (node_row, node_column),
+                    (node_row + 1, node_column),
+                    Direction::Left,
+                ));
             }
 
             if !region_hashset.contains(&right_neighbour) {
                 fence_points.insert((node_row, node_column + 1));
                 fence_points.insert((node_row + 1, node_column + 1));
-                fence_edges.insert(((node_row, node_column + 1), (node_row + 1, node_column + 1)));
+                fence_edges.insert((
+                    (node_row, node_column + 1),
+                    (node_row + 1, node_column + 1),
+                    Direction::Right,
+                ));
             }
         }
 
-        println!("Fence points for region {region_hashset:?} are {fence_points:?}");
-        println!("Fence edges for region {region_hashset:?} are {fence_edges:?}");
+        // println!("Fence points for region {region_hashset:?} are {fence_points:?}");
+        // println!("Fence edges for region {region_hashset:?} are {fence_edges:?}");
         let mut combined_fence_edges = HashSet::new();
         let mut fence_points_vec = fence_points.iter().copied().collect::<Vec<_>>();
 
         while let Some(point) = fence_points_vec.pop() {
-            println!("Checking point {point:?}");
-            // Go left
-            let mut left_point = point;
-            while fence_edges.contains(&((left_point.0, left_point.1 - 1), left_point)) {
-                left_point = (left_point.0, left_point.1 - 1);
-            }
-            println!("Went left to point {left_point:?}");
+            // println!("Checking point {point:?}");
+            for direction in [Direction::Up, Direction::Down] {
+                // Go left
+                let mut left_point = point;
+                while fence_edges.contains(&(
+                    (left_point.0, left_point.1 - 1),
+                    left_point,
+                    direction,
+                )) {
+                    left_point = (left_point.0, left_point.1 - 1);
+                }
+                // println!("Went left to point {left_point:?}");
 
-            // Go right
-            let mut right_point = point;
-            while fence_edges.contains(&(right_point, (right_point.0, right_point.1 + 1))) {
-                right_point = (right_point.0, right_point.1 + 1);
-            }
-            println!("Went right to point {right_point:?}");
+                // Go right
+                let mut right_point = point;
+                while fence_edges.contains(&(
+                    right_point,
+                    (right_point.0, right_point.1 + 1),
+                    direction,
+                )) {
+                    right_point = (right_point.0, right_point.1 + 1);
+                }
+                // println!("Went right to point {right_point:?}");
 
-            if right_point != left_point {
-                combined_fence_edges.insert((left_point, right_point));
+                if right_point != left_point {
+                    combined_fence_edges.insert((left_point, right_point));
+                }
             }
 
-            // Go up
-            let mut up_point = point;
-            while fence_edges.contains(&((up_point.0 - 1, up_point.1), up_point)) {
-                up_point = (up_point.0 - 1, up_point.1);
-            }
-            println!("Went up to point {up_point:?}");
+            for direction in [Direction::Right, Direction::Left] {
+                // Go up
+                let mut up_point = point;
+                while fence_edges.contains(&((up_point.0 - 1, up_point.1), up_point, direction)) {
+                    up_point = (up_point.0 - 1, up_point.1);
+                }
+                // println!("Went up to point {up_point:?}");
 
-            // Go down
-            let mut down_point = point;
-            while fence_edges.contains(&(down_point, (down_point.0 + 1, down_point.1))) {
-                down_point = (down_point.0 + 1, down_point.1);
-            }
-            println!("Went down to point {down_point:?}");
+                // Go down
+                let mut down_point = point;
+                while fence_edges.contains(&(
+                    down_point,
+                    (down_point.0 + 1, down_point.1),
+                    direction,
+                )) {
+                    down_point = (down_point.0 + 1, down_point.1);
+                }
+                // println!("Went down to point {down_point:?}");
 
-            if up_point != down_point {
-                combined_fence_edges.insert((up_point, down_point));
+                if up_point != down_point {
+                    combined_fence_edges.insert((up_point, down_point));
+                }
             }
         }
 
-        println!("Combined fence edges for region {region_hashset:?} are {combined_fence_edges:?}");
-        println!("So I'm going to add {} and {} for {}", region_size, combined_fence_edges.len(), region_size * combined_fence_edges.len());
+        // println!("Combined fence edges for region {region_hashset:?} are {combined_fence_edges:?}");
+        // println!("So I'm going to add {} and {} for {}", region_size, combined_fence_edges.len(), region_size * combined_fence_edges.len());
         result += region_size * combined_fence_edges.len();
     }
 
-    result as u64 // 906824 too low!
+    result as u64
 }
 
 #[cfg(test)]
@@ -226,15 +270,15 @@ mod tests {
     use rstest::rstest;
 
     #[rstest]
-    #[case(true, 140)]
+    #[case(true, 1930)]
     #[case(false, 1488414)]
     fn test_part_1(#[case] is_test: bool, #[case] expected: u64) {
         assert_eq!(expected, part_1(get_file_path(is_test, 12, None)));
     }
 
     #[rstest]
-    #[case(true, 0)]
-    #[case(false, 0)]
+    #[case(true, 1206)]
+    #[case(false, 911750)]
     fn test_part_2(#[case] is_test: bool, #[case] expected: u64) {
         assert_eq!(expected, part_2(get_file_path(is_test, 12, None)));
     }
