96488dd2cda4df9e30949f6449b34da1a383a480
mor.verbin <mor.verbin@coolblue.nl>
2024-12-15 10:57:10 +0100

    Day 15: Part 1, and some regrets of life choices for part 2.

--- src/day_15.rs
+++ src/day_15.rs
@@ -1,21 +1,54 @@
 use crate::file_utilities::read_two_chunks;
 use crate::map_utilities::{Direction, Point};
+use std::collections::HashSet;
 
 use itertools::Itertools;
 
 struct Map {
     robot: Point,
-    walls: Vec<Point>,
-    boxes: Vec<Point>,
+    walls: HashSet<Point>,
+    boxes: HashSet<Point>,
 }
 
 impl Map {
     fn new() -> Self {
         Self {
             robot: Point::new(0, 0),
-            walls: vec![],
-            boxes: vec![],
+            walls: HashSet::new(),
+            boxes: HashSet::new(),
         }
+    }
+
+    fn move_robot(&mut self, direction: Direction) {
+        let desired_spot = self.robot.unbound_neighbour(direction);
+
+        // Wall - we're done.
+        if self.walls.contains(&desired_spot) {
+            return;
+        }
+
+        // No wall, and no box - we move.
+        if !self.boxes.contains(&desired_spot) {
+            self.robot = desired_spot;
+            return;
+        }
+
+        // Box - keep going until there's no more box.
+        let mut target = desired_spot;
+
+        while self.boxes.contains(&target) {
+            target = target.unbound_neighbour(direction);
+        }
+
+        // If target is a wall, you're stuck.
+        if self.walls.contains(&target) {
+            return;
+        }
+
+        // If target is free, you move the desired_spot box there and move the robot.
+        self.boxes.remove(&desired_spot);
+        self.boxes.insert(target);
+        self.robot = desired_spot;
     }
 }
 
@@ -28,12 +61,14 @@ fn parse_data(file_path: String) -> (Map, Vec<Direction>) {
         for (column_index, character) in row.chars().enumerate() {
             match character {
                 '.' => continue,
-                '#' => map
-                    .walls
-                    .push(Point::new(row_index as isize, column_index as isize)),
-                'O' => map
-                    .boxes
-                    .push(Point::new(row_index as isize, column_index as isize)),
+                '#' => {
+                    map.walls
+                        .insert(Point::new(row_index as isize, column_index as isize));
+                }
+                'O' => {
+                    map.boxes
+                        .insert(Point::new(row_index as isize, column_index as isize));
+                }
                 '@' => map.robot = Point::new(row_index as isize, column_index as isize),
                 _ => panic!("Got an invalid map thingie."),
             }
@@ -65,11 +100,18 @@ pub fn run(file_path: String, part: i32) -> usize {
 }
 
 fn part_1(file_path: String) -> usize {
-    let (map, directions) = parse_data(file_path);
+    let (mut map, directions) = parse_data(file_path);
     // println!("robot {:?}, walls {:?}, boxes {:?}", map.robot, map.walls, map.boxes);
     // println!("{directions:?}");
 
-    0
+    for direction in directions {
+        map.move_robot(direction);
+    }
+
+    map.boxes
+        .into_iter()
+        .map(|point| (point.row * 100) as usize + point.column as usize)
+        .sum()
 }
 
 fn part_2(file_path: String) -> usize {
@@ -84,8 +126,8 @@ mod tests {
     use rstest::rstest;
 
     #[rstest]
-    #[case(true, 0)]
-    #[case(false, 0)]
+    #[case(true, 10092)]
+    #[case(false, 1538871)]
     fn test_part_1(#[case] is_test: bool, #[case] expected: usize) {
         assert_eq!(expected, part_1(get_file_path(is_test, 15, None)));
     }
