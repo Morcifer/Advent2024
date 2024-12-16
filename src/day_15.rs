fc2e47d93d822c16f86703d23f5edd93618594e2
mor.verbin <mor.verbin@coolblue.nl>
2024-12-16 22:27:57 +0100

    Day 15: Much cleaner! Took a while because I had a missing semicolon in the match statement... Stupid Rust!

--- src/day_15.rs
+++ src/day_15.rs
@@ -4,304 +4,193 @@ use std::collections::HashSet;
 
 use itertools::Itertools;
 
+#[derive(Copy, Clone, Debug, Eq, Hash, PartialEq)]
+enum Thing {
+    Empty,
+    Wall,
+    Box,
+    LeftBox,
+    RightBox,
+    Robot,
+}
+
+impl Thing {
+    fn from_char(c: char) -> Thing {
+        match c {
+            '#' => Thing::Wall,
+            'O' => Thing::Box,
+            '@' => Thing::Robot,
+            '.' => Thing::Empty,
+            // '[' => Thing::LeftBox, // Should not happen.
+            // ']' => Thing::RightBox, // Should not happen.
+            _ => panic! {"Did not expect this!"},
+        }
+    }
+
+    fn from_char_bigger(c: char) -> Vec<Thing> {
+        match c {
+            '#' => vec![Thing::Wall, Thing::Wall],
+            'O' => vec![Thing::LeftBox, Thing::RightBox],
+            '@' => vec![Thing::Robot, Thing::Empty],
+            '.' => vec![Thing::Empty, Thing::Empty],
+            _ => panic! {"Did not expect this!"},
+        }
+    }
+
+    fn to_char(&self) -> char {
+        match self {
+            Thing::Wall => '#',
+            Thing::Box => 'O',
+            Thing::Robot => '@',
+            Thing::Empty => '.',
+            Thing::LeftBox => '[',
+            Thing::RightBox => ']',
+        }
+    }
+}
+
 struct Map {
     robot: Point,
-    walls: HashSet<Point>,
-    boxes: HashSet<Point>,
-    large_boxes_lefts: HashSet<Point>,
-    large_boxes_rights: HashSet<Point>,
+    map: Vec<Vec<Thing>>,
 }
 
 impl Map {
-    fn new() -> Self {
-        Self {
-            robot: Point::new(0, 0),
-            walls: HashSet::new(),
-            boxes: HashSet::new(),
-            large_boxes_lefts: HashSet::new(),
-            large_boxes_rights: HashSet::new(),
-        }
-    }
+    fn new(lines: Vec<String>, bigger: bool) -> Self {
+        let mut map = vec![];
+        let mut robot = Point::new(0, 0);
 
-    fn print_to_console(&self) {
-        let max_row = self.walls.iter().map(|w| w.row).max().unwrap();
-        let max_column = self.walls.iter().map(|w| w.column).max().unwrap();
+        for (row, line) in lines.into_iter().enumerate() {
+            let mut map_line = vec![];
 
-        let mut map = (0..=max_row)
-            .map(|_| (0..=max_column).map(|_| '.').collect_vec())
-            .collect_vec();
+            for (column, char) in line.chars().enumerate() {
+                if bigger {
+                    let things = Thing::from_char_bigger(char);
 
-        for wall in self.walls.iter() {
-            map[wall.row as usize][wall.column as usize] = '#';
-        }
+                    if things[0] == Thing::Robot {
+                        robot = Point::new(row as isize, (column * 2) as isize);
+                    }
 
-        for box_ in self.boxes.iter() {
-            map[box_.row as usize][box_.column as usize] = 'O';
-        }
-
-        for box_ in self.large_boxes_lefts.iter() {
-            map[box_.row as usize][box_.column as usize] = '[';
-        }
-
-        for box_ in self.large_boxes_rights.iter() {
-            map[box_.row as usize][box_.column as usize] = ']';
-        }
-
-        map[self.robot.row as usize][self.robot.column as usize] = '@';
-
-        for row in map {
-            println!("{}", row.iter().collect::<String>());
-        }
-    }
-
-    fn make_map_bigger(&mut self) {
-        self.robot = Point::new(self.robot.row, 2 * self.robot.column);
-
-        self.walls = self
-            .walls
-            .iter()
-            .copied()
-            .flat_map(|point| {
-                vec![
-                    Point::new(point.row, 2 * point.column),
-                    Point::new(point.row, 2 * point.column + 1),
-                ]
-            })
-            .collect();
-
-        self.large_boxes_lefts = self
-            .boxes
-            .iter()
-            .copied()
-            .map(|point| Point::new(point.row, 2 * point.column))
-            .collect();
-
-        self.large_boxes_rights = self
-            .boxes
-            .iter()
-            .copied()
-            .map(|point| Point::new(point.row, 2 * point.column + 1))
-            .collect();
-
-        self.boxes.clear();
-    }
-
-    fn move_robot(&mut self, direction: Direction) {
-        let desired_spot = self.robot.unbound_neighbour(direction);
-
-        // Wall - we're done.
-        if self.walls.contains(&desired_spot) {
-            return;
-        }
-
-        // No wall, and no box - we move.
-        if !self.boxes.contains(&desired_spot) {
-            self.robot = desired_spot;
-            return;
-        }
-
-        // Box - keep going until there's no more box.
-        let mut target = desired_spot;
-
-        while self.boxes.contains(&target) {
-            target = target.unbound_neighbour(direction);
-        }
-
-        // If target is a wall, you're stuck.
-        if self.walls.contains(&target) {
-            return;
-        }
-
-        // If target is free, you move the desired_spot box there and move the robot.
-        self.boxes.remove(&desired_spot);
-        self.boxes.insert(target);
-        self.robot = desired_spot;
-    }
-
-    fn move_big_robot(&mut self, direction: Direction) {
-        // if direction == Direction::Up {
-        //     println!("Stop here please!");
-        // }
-        let desired_spot = self.robot.unbound_neighbour(direction);
-
-        // Wall - we're done.
-        if self.walls.contains(&desired_spot) {
-            return;
-        }
-
-        // No wall, and no box - we move.
-        if !self.large_boxes_lefts.contains(&desired_spot)
-            && !self.large_boxes_rights.contains(&desired_spot)
-        {
-            self.robot = desired_spot;
-            return;
-        }
-
-        // Box - keep going until there's no more boxes.
-        let mut spots_to_move = vec![]; // Vector of hashsets, might not need it.
-        let mut box_lefts_to_move = HashSet::new();
-
-        match direction {
-            Direction::Right => spots_to_move.push(
-                vec![desired_spot.unbound_neighbour(Direction::Right)]
-                    .into_iter()
-                    .collect::<HashSet<_>>(),
-            ),
-            Direction::Left => spots_to_move.push(
-                vec![desired_spot.unbound_neighbour(Direction::Left)]
-                    .into_iter()
-                    .collect::<HashSet<_>>(),
-            ),
-            _ => {
-                if self.large_boxes_lefts.contains(&desired_spot) {
-                    spots_to_move.push(
-                        vec![
-                            desired_spot,
-                            desired_spot.unbound_neighbour(Direction::Right),
-                        ]
-                        .into_iter()
-                        .collect::<HashSet<_>>(),
-                    );
-                    // box_lefts_to_move.push(desired_spot);
+                    map_line.extend(things);
                 } else {
-                    spots_to_move.push(
-                        vec![
-                            desired_spot,
-                            desired_spot.unbound_neighbour(Direction::Left),
-                        ]
-                        .into_iter()
-                        .collect::<HashSet<_>>(),
-                    );
-                    // box_lefts_to_move.push(desired_spot.unbound_neighbour(Direction::Left));
+                    let thing = Thing::from_char(char);
+                    if thing == Thing::Robot {
+                        robot = Point::new(row as isize, column as isize);
+                    }
+                    map_line.push(thing);
+                }
+            }
+
+            map.push(map_line);
+        }
+
+        Self { map, robot }
+    }
+
+    fn get_boxes(&self) -> Vec<Point> {
+        let mut result = vec![];
+
+        for (i, row) in self.map.iter().enumerate() {
+            for (j, thing) in row.iter().enumerate() {
+                match thing {
+                    Thing::Box | Thing::LeftBox => {
+                        result.push(Point::new(i as isize, j as isize));
+                    }
+                    _ => continue,
                 };
             }
         }
 
+        result
+    }
+
+    fn print_to_console(&self) {
+        for row in &self.map {
+            let to_print = row.iter().map(Thing::to_char).join("");
+            println!("{to_print}");
+        }
+    }
+
+    fn move_robot(&mut self, direction: Direction) {
+        let mut spots_to_move = vec![vec![self.robot].into_iter().collect::<HashSet<_>>()];
         let mut last_spots_to_move = spots_to_move.last().unwrap().clone();
 
-        while last_spots_to_move.iter().any(|spot| {
-            self.large_boxes_lefts.contains(spot) || self.large_boxes_rights.contains(spot)
-        }) {
-            // println!("Need to move spots {last_spots_to_move:?} to {direction:?}");
-
+        while last_spots_to_move
+            .iter()
+            .map(|spot| spot.unbound_neighbour(direction))
+            .any(|spot| self.map[spot.row()][spot.column()] != Thing::Empty)
+        {
             let mut new_spots_to_move = HashSet::new();
 
             for spot in last_spots_to_move {
-                // Update left-sides for spots
-                if self.large_boxes_lefts.contains(&spot) {
-                    box_lefts_to_move.insert(spot);
-                } else if self.large_boxes_rights.contains(&spot) {
-                    box_lefts_to_move.insert(spot.unbound_neighbour(Direction::Left));
-                }
+                let new_spot = spot.unbound_neighbour(direction);
+                let spot_thing = self.map[new_spot.row()][new_spot.column()];
 
                 match direction {
-                    Direction::Right => {
-                        new_spots_to_move.insert(spot.unbound_neighbour(Direction::Right));
+                    Direction::Right | Direction::Left => {
+                        match spot_thing {
+                            Thing::Wall => {
+                                // println!("There's nothing we can do, {new_spot:?} is a wall!");
+                                return;
+                            }
+                            Thing::Empty => {
+                                continue;
+                            }
+                            _ => {
+                                new_spots_to_move.insert(new_spot);
+                            }
+                        }
                     }
-                    Direction::Left => {
-                        new_spots_to_move.insert(spot.unbound_neighbour(Direction::Left));
-                    }
-                    _ => {
-                        if self.large_boxes_lefts.contains(&spot) {
-                            new_spots_to_move.insert(spot.unbound_neighbour(direction));
-                            new_spots_to_move.insert(
-                                spot.unbound_neighbour(direction)
-                                    .unbound_neighbour(Direction::Right),
-                            );
-                        } else if self.large_boxes_rights.contains(&spot) {
-                            new_spots_to_move.insert(spot.unbound_neighbour(direction));
-                            new_spots_to_move.insert(
-                                spot.unbound_neighbour(direction)
-                                    .unbound_neighbour(Direction::Left),
-                            );
-                        };
+                    Direction::Up | Direction::Down => {
+                        match spot_thing {
+                            Thing::Wall => {
+                                // println!("There's nothing we can do, {new_spot:?} is a wall!");
+                                return;
+                            }
+                            Thing::Empty => {
+                                continue;
+                            }
+                            Thing::Box => {
+                                new_spots_to_move.insert(new_spot);
+                            }
+                            Thing::LeftBox => {
+                                new_spots_to_move.insert(new_spot);
+                                new_spots_to_move
+                                    .insert(new_spot.unbound_neighbour(Direction::Right));
+                            }
+                            Thing::RightBox => {
+                                new_spots_to_move.insert(new_spot);
+                                new_spots_to_move
+                                    .insert(new_spot.unbound_neighbour(Direction::Left));
+                            }
+                            any => panic!("I got {any:?} but I shouldn't have!"),
+                        }
                     }
                 }
-
-                // // And update for new spots.
-                // let new_spot = spot.unbound_neighbour(direction);
-                // new_spots_to_move.insert(new_spot);
             }
 
             spots_to_move.push(new_spots_to_move.clone());
             last_spots_to_move = new_spots_to_move.clone();
         }
 
-        if spots_to_move
-            .into_iter()
-            .flatten()
-            .any(|spot| self.walls.contains(&spot))
-        {
-            // any sort of wall would get us stuck.
-            // println!("Hit a wall when going {direction:?}");
-            return;
-        } else {
-            // println!("Can indeed go {direction:?}");
+        // Move in reverse order, but skip the robot line and the wall line.
+        // println!("{spots_to_move:?}");
+        for last_spots_to_move in spots_to_move.iter().rev() {
+            for spot in last_spots_to_move {
+                let spot_to_move_to = spot.unbound_neighbour(direction);
+                self.map[spot_to_move_to.row()][spot_to_move_to.column()] =
+                    self.map[spot.row()][spot.column()];
+                self.map[spot.row()][spot.column()] = Thing::Empty;
+            }
         }
 
-        // If target is free, you move the desired_spot box there and move the robot.
-        // But when moving the boxes, do it in reverse or you'll be in trouble, I think.
-        self.robot = desired_spot;
-
-        let old_box_lefts_to_move = box_lefts_to_move.clone();
-        // println!("Need to move left-box {old_box_lefts_to_move:?}");
-
-        let old_box_rights_to_move = box_lefts_to_move
-            .iter()
-            .copied()
-            .map(|spot| spot.unbound_neighbour(Direction::Right))
-            .collect_vec();
-
-        let new_box_lefts = box_lefts_to_move
-            .into_iter()
-            .map(|spot| spot.unbound_neighbour(direction))
-            .collect_vec();
-        // println!("Will move them to {new_box_lefts:?}");
-
-        let new_box_rights = new_box_lefts
-            .iter()
-            .copied()
-            .map(|spot| spot.unbound_neighbour(Direction::Right))
-            .collect_vec();
-
-        for old in old_box_lefts_to_move {
-            self.large_boxes_lefts.remove(&old);
-        }
-        for old in old_box_rights_to_move {
-            self.large_boxes_rights.remove(&old);
-        }
-
-        for new in new_box_lefts {
-            self.large_boxes_lefts.insert(new);
-        }
-        for new in new_box_rights {
-            self.large_boxes_rights.insert(new);
-        }
+        self.robot = self.robot.unbound_neighbour(direction);
     }
 }
 
-fn parse_data(file_path: String) -> (Map, Vec<Direction>) {
+fn parse_data(file_path: String, bigger: bool) -> (Map, Vec<Direction>) {
     let (map_lines, direction_lines) = read_two_chunks(file_path);
 
-    let mut map = Map::new();
-
-    for (row_index, row) in map_lines.into_iter().enumerate() {
-        for (column_index, character) in row.chars().enumerate() {
-            match character {
-                '.' => continue,
-                '#' => {
-                    map.walls
-                        .insert(Point::new(row_index as isize, column_index as isize));
-                }
-                'O' => {
-                    map.boxes
-                        .insert(Point::new(row_index as isize, column_index as isize));
-                }
-                '@' => map.robot = Point::new(row_index as isize, column_index as isize),
-                _ => panic!("Got an invalid map thingie."),
-            }
-        }
-    }
+    let map = Map::new(map_lines, bigger);
 
     let directions = direction_lines
         .into_iter()
@@ -328,35 +217,36 @@ pub fn run(file_path: String, part: i32) -> usize {
 }
 
 fn part_1(file_path: String) -> usize {
-    let (mut map, directions) = parse_data(file_path);
-
-    // println!("robot {:?}, walls {:?}, boxes {:?}", map.robot, map.walls, map.boxes);
-    // println!("{directions:?}");
+    let (mut map, directions) = parse_data(file_path, false);
     map.print_to_console();
 
     for direction in directions {
+        // println!("Moving {direction:?}:");
         map.move_robot(direction);
+        // map.print_to_console();
     }
 
-    map.boxes
+    map.print_to_console();
+
+    map.get_boxes()
         .into_iter()
         .map(|point| (point.row * 100) as usize + point.column as usize)
         .sum()
 }
 
 fn part_2(file_path: String) -> usize {
-    let (mut map, directions) = parse_data(file_path);
-    map.make_map_bigger();
+    let (mut map, directions) = parse_data(file_path, true);
     map.print_to_console();
 
     for direction in directions {
-        // println!("Doing direction {direction:?}");
-        map.move_big_robot(direction);
+        // println!("Moving {direction:?}:");
+        map.move_robot(direction);
         // map.print_to_console();
-        // break;
     }
 
-    map.large_boxes_lefts
+    map.print_to_console();
+
+    map.get_boxes()
         .into_iter()
         .map(|point| (point.row * 100) as usize + point.column as usize)
         .sum()
